2dfcc83a7d0bb0b3a0f6a6d3b2bfb23473e9d4db
Your Name <you@example.com>
2023-03-21 14:27:11 -0500

    add images foods

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -14,6 +14,30 @@ export class FoodService {
       'assets/images/arroz_verde.jpg',
       'assets/images/brocoli_saltado.jpg',
       'assets/images/caldo_de_gallina.jpg',
+      'assets/images/carapulcra.jpg',
+      'assets/images/causa_rellena.jpg',
+      'assets/images/champu.jpg',
+      'assets/images/chiuche.png',
+      'assets/images/colifor_saltado.jpg',
+      'assets/images/dulce_de_trigo.jpg',
+      'assets/images/gelatina.jpg',
+      'assets/images/humitas.jpg',
+      'assets/images/jugo_fresa.jpeg',
+      'assets/images/jugo_guanabana.png',
+      'assets/images/jugo_mango.jpg',
+      'assets/images/jugo_mora.jpg',
+      'assets/images/jugo_papaya.jpeg',
+      'assets/images/panqueques_de_avena.JPG',
+      'assets/images/papa_rellena.jpeg',
+      'assets/images/picarones.jpg',
+      'assets/images/pollo_a_la_plancha.jpg',
+      'assets/images/pure_de_espinaca.jpg',
+      'assets/images/sopa_de_paico.jpg',
+      'assets/images/sudado_pescado.jpg',
+      'assets/images/tallarin_rojo.jpg',
+      'assets/images/tallarin_verde.jpg',
+
+
     ]
   }
 }
