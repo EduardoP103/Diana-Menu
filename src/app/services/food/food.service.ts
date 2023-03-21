ea1e8aff8008884b32167296205d59be0db5930b
Your Name <you@example.com>
2023-03-21 14:16:26 -0500

    adding products list images

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -12,6 +12,8 @@ export class FoodService {
       'assets/images/aguadito.jpg',
       'assets/images/aji_de_gallina.jpeg',
       'assets/images/arroz_verde.jpg',
+      'assets/images/brocoli_saltado.jpg',
+      'assets/images/caldo_de_gallina.jpg',
     ]
   }
 }
