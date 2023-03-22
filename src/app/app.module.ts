9439b9474942a8631542af4073ab8c89363ce415
Your Name <you@example.com>
2023-03-21 22:46:32 -0500

    adding food details to the cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { RatingComponent } from 'ng-starrating/lib/rating.component';
+import { StarRatingComponent } from 'ng-starrating';
+import {RatingModule} from 'ng-starrating';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
